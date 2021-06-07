ecde0a13dda67ac370eb850b30d17876aa2acb80
Ian F <ian.fiddeman@gmail.com>
2021-06-07 22:08:34 +0100

    portal for overlay now working

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 
 const Backdrop = props => {
@@ -28,10 +28,16 @@ const ModalOverlay = props =>{
 const ErrorModal = props => {
     return (
     <React.Fragment>
-        {ReactDom.createPortal(<Backdrop onConfirm={props.onConfirm} />, 
+        {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, 
         document.getElementById('backdrop-root'))}
-        {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>)
-    }
+        {ReactDOM.createPortal(
+          <ModalOverlay 
+            title={props.title} 
+            message={props.message} 
+            onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+        )}
       </React.Fragment>
     );
 };
